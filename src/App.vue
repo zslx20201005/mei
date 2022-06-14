0c2b58795fe0ff958fa1d31c8a5c7da464c0ed78
zhang.sen <zhang.sen@jnetdata.com>
2022-06-14 13:34:53 +0800

    src

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,84 @@ import { $computed, $ref } from 'vue/macros'
 const el = $ref<HTMLCanvasElement>()
 const ctx = $computed(() => el!.getContext('2d')!)
 
+const WIDTH = 600
+const HEIGHT = 600
+
+interface Point {
+  x: number
+  y: number
+}
+
+interface Branch {
+  start: Point
+  length: number
+  theta: number // 夹角
+}
+
 function init() {
   ctx.strokeStyle = '#fff'
+
+  step({
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 20,
+    theta: -Math.PI / 2,
+  })
+}
+
+const pendingTasks: Function[] = []
+
+function step(b: Branch, depth = 0) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+
+  if (depth < 4 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta - 0.3 * Math.random(),
+    }, depth + 1))
+  }
+  if (depth < 4 || Math.random() < 0.5) {
+    pendingTasks.push(() => step({
+      start: end,
+      length: b.length + (Math.random() * 10 - 5),
+      theta: b.theta + 0.3 * Math.random(),
+    }, depth + 1))
+  }
+}
+
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+let framesCount = 0
+function startFrame() {
+  requestAnimationFrame(() => {
+    framesCount += 1
+    if (framesCount % 3 === 0)
+      frame()
+    startFrame()
+  })
+}
+startFrame()
+
+function lineTo(p1: Point, p2: Point) {
   ctx.beginPath()
-  ctx.moveTo(30, 50)
-  ctx.lineTo(150, 100)
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
   ctx.stroke()
 }
 
-function line(x1: number, y1: number, x2: number, y2: number) {
-  ctx.beginPath()
-  ctx.moveTo(x1, y1)
-  ctx.lineTo(x2, y2)
+function getEndPoint(b: Branch) {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
+  }
+}
+
+function drawBranch(b: Branch) {
+  lineTo(b.start, getEndPoint(b))
 }
 
 onMounted(() => {
